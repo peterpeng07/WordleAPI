85db12368fb3bfb4ffb582eea076986a8bfaa62f
Peter Peng <yser.pc@gmail.com>
2022-02-07 21:28:58 -0500

    added items in lists

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import flask
 from flask import request
 
+
+def load_words(fname):
+    with open(fname) as word_file:
+        valid_words = set(word_file.read().split())
+
+    return valid_words
+
+
 app = flask.Flask(__name__)
 
 
@@ -16,12 +24,47 @@ def home():
     includeList = request_data['includeList']
     excludeList = request_data['excludeList']
 
+    words = load_words('words_5.txt')
+    req = ''
+    include = []
+    exclude = []
+    result = []
+
+    if first != '':
+        req += first
+    else:
+        req += '.'
+    if second != '':
+        req += second
+    else:
+        req += '.'
+    if third != '':
+        req += third
+    else:
+        req += '.'
+    if fourth != '':
+        req += fourth
+    else:
+        req += '.'
+    if fifth != '':
+        req += fifth
+    else:
+        req += '.'
+
+    for w in includeList:
+        include.append((w['letter'], w['position']))
+
+    for i in excludeList:
+        exclude.append(i)
+
     return '''
-          1: {}
-          2: {}
-          3: {}
-          4: {}
-          5: {}
+          req: {}
           include: {}
           exclude: {}
-    '''.format(first, second, third, fourth, fifth, includeList, excludeList)
+    '''.format(req, include, exclude)
+
+ # 1: {}
+    # 2: {}
+    # 3: {}
+    # 4: {}
+    # 5: {}
