6e27421a458e52a6073fdd28d37aeb1c54226983
Peter Peng <yser.pc@gmail.com>
2022-02-16 16:49:45 -0500

    added CORS

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask
 from flask import request
 from flask import jsonify
+from flask_cors import CORS
 import re
 
 
@@ -12,6 +13,7 @@ def load_words(fname):
 
 
 app = flask.Flask(__name__)
+CORS(app)
 
 
 @app.route('/', methods=['POST'])
