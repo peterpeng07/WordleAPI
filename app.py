c7ff99ab560e83c78c5e2d92ff6606542a8dead6
Peter Peng <yser.pc@gmail.com>
2022-02-16 22:03:55 -0500

    test'

--- app.py
+++ app.py
@@ -19,6 +19,11 @@ def home():
     return "Welcome to Wordle Solver API!"
 
 
+@app.route('/test', methods=['POST'])
+def test():
+    return jsonify(["test", "test1"])
+
+
 @app.route('/', methods=['POST'])
 def search():
     request_data = request.get_json()
